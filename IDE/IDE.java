69085473203449088d476fd4ee89f42b456eb795
SeaDo <99972571+SeaDomain@users.noreply.github.com>
2025-10-26 21:46:52 +0900

    Add files via upload

--- IDE/IDE.java
+++ IDE/IDE.java
@@ -1,5 +1,4 @@
 import java.io.*;
-import java.nio.file.Files;
 import java.util.*;
 
 //main 객체
@@ -12,16 +11,16 @@ public class IDE {
 //GUI 개발할 때를 대비하여 CLI 부분을 따로 떼놓음.
 //기능을 추가하려면 loop()의 switch문에서 동작 코드를, print_menu()에서 기능및 번호 설명 코드를 추가할것
 class CLI {
-    JavaFile Jfile; // 업로드된 파일 정보를 저장할 변수
+    JavaFile Jfile;
 
     //Constructor
     CLI() {
         loop();
     }
 
-        //CLI loop
-        void loop() {
-            Scanner scan = new Scanner(System.in); // [수정] 스캐너는 한 번만 생성
+    //CLI loop
+    void loop() {
+        Scanner scan = new Scanner(System.in);
         while (true) {
             print_menu();
             int action = scan.nextInt();
@@ -32,22 +31,12 @@ class CLI {
                     Jfile = upload_file(path);
                     break;
                 case 2:
-                    if (Jfile == null) {
-                        System.out.println("먼저 Java 파일을 업로드해주세요. (메뉴 1)");
-                        break;
-                    }
-                    compileJavaFile(Jfile);
+                    compile_JavaFile(Jfile);
                     break;
                 case 3:
-                    // [추가] 파일이 업로드되었는지 확인
-                    if (Jfile == null) {
-                        System.out.println("먼저 Java 파일을 업로드해주세요. (메뉴 1)");
-                        break;
-                    }
-                    runJavaFile(Jfile);
+                    run_JavaFile(Jfile);
                     break;
                 case 4:
-                    reset_JavaFile();
                     break;
                 case 5:
                     break;
@@ -83,69 +72,46 @@ class CLI {
         return new JavaFile(path);
     }
 
-    void compileJavaFile(JavaFile file) {
+    void compile_JavaFile(JavaFile file) {
         try {
             File javaFile = new File(file.path);
-
-            ProcessBuilder compile = new ProcessBuilder("javac", file.path);
-
-            compile.redirectErrorStream(true); // 오류 스트림을 표준 출력 스트림으로 합침
-
+            ProcessBuilder compile = new ProcessBuilder("javac", file.path); //.class
             Process compileProcess = compile.start();
+            java_FileOutput(compileProcess);
+            int exit_code = compileProcess.waitFor();
 
-            printProcessOutput("컴파일", compileProcess);
-
-            int exitCode = compileProcess.waitFor();
-
-            if (exitCode == 0) {
+            if (exit_code == 0) {
                 System.out.println("“compiled successfully …");
             } else {
-                System.out.println("컴파일에 실패했습니다. (종료 코드: " + exitCode + ")");
+                System.out.println("compile fail");
             }
 
-        } catch (IOException | InterruptedException e) { // [수정] InterruptedException 추가
-            System.out.println("컴파일 중 오류 발생:");
-            e.printStackTrace(); // [수정] 비어있는 catch 블록 대신 오류 로그 출력
+        } catch (Exception e) {
+            System.out.println("compile error");
+            
         }
     }
 
-    void runJavaFile(JavaFile file) {
+    void run_JavaFile(JavaFile file) {
         try {
             File javaFile = new File(file.path);
-            File parentDir = javaFile.getParentFile(); // .java 파일이 위치한 디렉토리
-            String className = javaFile.getName().replace(".java", "");
-            File classFile = new File(parentDir, className + ".class");
-//            if (!classFile.exists()) {
-//                System.out.println(".class 파일이 존재하지 않습니다.");
-//                System.out.println("먼저 컴파일을 실행해주세요. (메뉴 2)");
-//                return;
-//            }
+            File parent_Dir = javaFile.getParentFile();
+            String java_class = javaFile.getName().replace(".java", "");
 
-            System.out.println(className + " 클래스를 실행합니다...");
+            ProcessBuilder run_class = new ProcessBuilder("java", java_class);
+            run_class.directory(parent_Dir);
 
-            ProcessBuilder run = new ProcessBuilder("java", className);
-            System.out.println(parentDir);
-            run.directory(parentDir); // [중요] .class 파일이 있는 디렉토리에서 실행
-            run.redirectErrorStream(true);
+            Process run_Process = run_class.start();
+            java_FileOutput(run_Process);
 
-            Process runProcess = run.start();
-            printProcessOutput("실행", runProcess);
-
-            int exitCode = runProcess.waitFor();
-            System.out.println("프로그램 종료 코드: " + exitCode);
-
-        } catch (IOException | InterruptedException e) { // [수정] InterruptedException 추가
-            System.out.println("실행 중 오류 발생:");
-            e.printStackTrace();
+        } catch (Exception e) {
+            System.out.println("run time error");
         }
     }
 
-    /**
-     * 프로세스의 표준 출력/오류 스트림을 읽어 콘솔에 출력합니다.
-     */
-    void printProcessOutput(String tag, Process process) throws IOException {
+    void java_FileOutput(Process process) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-{
+        {
             String line;
             while ((line = reader.readLine()) != null) {
                 System.out.println(line);
@@ -153,32 +119,6 @@ class CLI {
         }
     }
 
-    void reset_JavaFile() {
-        if (Jfile == null) {
-            System.out.println("초기화할 파일 정보가 없습니다.");
-            return;
-        }
-
-        String javaFilePath = Jfile.path;
-        // .java 확장자를 .class로 변경하여 .class 파일 경로 생성
-        String classFilePath = javaFilePath.substring(0, javaFilePath.lastIndexOf(".java")) + ".class";
-
-        File classFile = new File(classFilePath);
-
-        if (classFile.exists()) {
-            if (classFile.delete()) {
-                System.out.println(classFile.getName() + " 파일이 삭제되었습니다.");
-            } else {
-                System.out.println(classFile.getName() + " 파일 삭제에 실패했습니다.");
-            }
-        } else {
-            System.out.println(".class 파일이 존재하지 않습니다. (이미 Reset됨)");
-        }
-
-        // Jfile 참조를 null로 변경하여 초기화
-        Jfile = null;
-        System.out.println("업로드된 파일 정보가 초기화되었습니다.");
-    }
 }
 
 class JavaFile {
